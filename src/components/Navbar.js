4f0ad423a104f8efa22b1ab9cdd7c34d960d9596
HarshalBopaliya <132598460+HarshalBopaliya@users.noreply.github.com>
2024-07-26 18:30:59 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 // import { Link } from 'react-router-dom'
 // import { Link } from 'react-router-dom'
 
@@ -8,17 +9,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">{props.title}</a>
+      <Link className="navbar-brand" to="/">{props.title}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about"> {props.aboutText} </a>
+            <Link className="nav-link" to="/about"> {props.aboutText} </Link>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="/about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -42,8 +43,8 @@ export default function Navbar(props) {
           <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode("success")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
           <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode("warning")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
           <div className="bg-info rounded mx-2" onClick={()=>{props.toggleMode("info")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-white rounded mx-2" onClick={()=>{props.toggleMode("white")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode("dark")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
+          {/* <div className="bg-white rounded mx-2" onClick={()=>{props.toggleMode("white")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div> */}
+          {/* <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode("dark")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div> */}
         </div>
         {/* <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
