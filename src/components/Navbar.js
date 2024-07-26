31c5046cefdc07fd973a92d2fc2ff0e660c4ba71
HarshalBopaliya <132598460+HarshalBopaliya@users.noreply.github.com>
2024-07-26 18:36:24 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,160 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-// import { Link } from 'react-router-dom'
-// import { Link } from 'react-router-dom'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">{props.title}</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about"> {props.aboutText} </Link>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="/">Action</a></li>
-              <li><a className="dropdown-item" href="/">Another action</a></li>
-              <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href="/">Something else here</a></li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="/about" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <div className="d-flex">
-          <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode("primary")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode("danger")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-secondary rounded mx-2" onClick={()=>{props.toggleMode("secondary")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode("success")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode("warning")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          <div className="bg-info rounded mx-2" onClick={()=>{props.toggleMode("info")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div>
-          {/* <div className="bg-white rounded mx-2" onClick={()=>{props.toggleMode("white")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div> */}
-          {/* <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode("dark")}} style={{height:'30px',width:'30px' , cursor : 'pointer'}} ></div> */}
-        </div>
-        {/* <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-primary" type="submit">Search</button>
-        </form> */}
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                {" "}
+                {props.aboutText}{" "}
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/about"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="/about"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <div className="d-flex">
+            <div
+              className="bg-primary rounded mx-2"
+              onClick={() => {
+                props.toggleMode("primary");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+            <div
+              className="bg-danger rounded mx-2"
+              onClick={() => {
+                props.toggleMode("danger");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+            <div
+              className="bg-secondary rounded mx-2"
+              onClick={() => {
+                props.toggleMode("secondary");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+            <div
+              className="bg-success rounded mx-2"
+              onClick={() => {
+                props.toggleMode("success");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+            <div
+              className="bg-warning rounded mx-2"
+              onClick={() => {
+                props.toggleMode("warning");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+            <div
+              className="bg-info rounded mx-2"
+              onClick={() => {
+                props.toggleMode("info");
+              }}
+              style={{ height: "30px", width: "30px", cursor: "pointer" }}
+            ></div>
+          </div>
 
-        <div className={`form-check form-switch " text-${props.mode ==='light'?'dark':'light'} mx-2`}>
-          <input className="form-check-input" type="checkbox" role="switch" onClick={()=>{props.toggleMode(null)}} id="flexSwitchCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> Toggle Mode</label>
+          <div
+            className={`form-check form-switch " text-${
+              props.mode === "light" ? "dark" : "light"
+            } mx-2`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              onClick={() => {
+                props.toggleMode(null);
+              }}
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              {" "}
+              Toggle Mode
+            </label>
+          </div>
         </div>
-
-       
-            
       </div>
-    </div>
-  </nav>
-  )
+    </nav>
+  );
 }
 
-Navbar.propTypes={   title: PropTypes.string.isRequired,
-    aboutText: PropTypes.string.isRequired
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
-    title: 'dynamic role',
-    aboutText:'About'
-  };
+  title: "dynamic role",
+  aboutText: "About",
+};
